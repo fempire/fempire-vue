340dd7b4cfe5e830a8bc01e4406922abd8a8f4fc
tessaSAC <tessaSAC@users.noreply.github.com>
2018-10-09 01:20:43 -0400

    add scss & reset, move SpeakerCard to component & make list responsive
    - adds normalize
    - adds border-box global reset
    - moves SpeakerCard to own component
    - replaces materialize layout with CSS grid for simpler layout
    - adds sass-resources-loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">   
-    <md-toolbar>
-        <img src="./assets/logo.png">
-      <h1 class="md-title">Fempire Vue.js</h1>
-    </md-toolbar>
-    <router-view/>
+<div id="app">
+  <md-toolbar>
+      <img src="./assets/logo.png">
+    <h1 class="md-title">Fempire Vue.js</h1>
+  </md-toolbar>
+  <div class="fempire-content">
+    <router-view />
   </div>
+</div>
 </template>
 
 <script>
@@ -14,15 +16,33 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+$toolbarHeight: 48px;
+
 #app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-.md-toolbar img{
-  height: 45px;
-  margin-right: 15px;
+
+.md-toolbar {
+  height: $toolbarHeight;
+
+  img {
+    height: 45px;
+    margin-right: 15px;
+  }
+}
+
+.fempire-content {
+  height: calc(100% - #{ $toolbarHeight });
+  padding: 3vh 2vw;
+  overflow-x: hidden;
+  overflow-y: auto;
 }
 </style>
---
+++ src/components/SpeakerCard.vue
@@ -0,0 +1,87 @@
+<template>
+<md-card>
+  <md-card-header>
+    <h3>{{ name }}</h3>
+  </md-card-header>
+  <md-card-content>
+
+    <div>
+      {{ twitter }}
+    </div>
+
+    <div v-if="website">
+      <a v-bind:href="website" target="_blank">
+        {{ website }}
+      </a>
+    </div>
+
+    <div>
+      Topics:
+      <ul>
+        <li v-for="topic in topics" :key="topic">
+          {{ topic }}
+        </li>
+      </ul>
+    </div>
+    <div v-if="location">
+      Location: {{ location }}
+    </div>
+
+    <div>
+      Languages:
+      <ul>
+        <li v-for="language in languages" :key="language">
+          {{ language }}
+        </li>
+      </ul>
+    </div>
+  </md-card-content>
+</md-card>
+</template>
+
+<script>
+export default {
+  props: {
+    languages: {
+      required: false,
+      type: Array,
+    },
+
+    location: {
+      required: true,
+      type: String,
+    },
+
+    name: {
+      required: true,
+      type: String,
+    },
+
+    topics: {
+      required: false,
+      type: Array,
+    },
+
+    twitter: {
+      required: false,
+      type: String,
+    },
+
+    website: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.md-theme-default.md-card {
+  width: 100%;
+}
+.md-card-header{
+    background-color: #dedede;
+    padding: 0px 16px;
+}
+</style>
